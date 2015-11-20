1a4710c00d3d5ca8b77299f91e3c32c05f15e083
Jose Manuel Peso <jmpeso@gmail.com>
2015-11-20 07:53:09 +0100

    first test running with mocha and chai

--- test/components/ContentHeader.jsx
+++ test/components/ContentHeader.jsx
@@ -15,12 +15,14 @@ describe('ContentHeader component', () => {
     subtitle: 'My subtitle',
   }
 
-  var sandbox, contentheader;
+  let sandbox, contentheader, h1, small, section;
 
   beforeEach(() => {
     sandbox = sinon.sandbox.create()
     contentheader = TestUtils.renderIntoDocument(<ContentHeader {...data} />)
-    h1 = TestUtils.scryRenderedDOMComponentsWithTag(contentheader, 'h1')
+    h1 = TestUtils.findRenderedDOMComponentWithTag(contentheader, 'h1')
+    small = TestUtils.findRenderedDOMComponentWithTag(contentheader, 'small')
+    section = TestUtils.findRenderedDOMComponentWithTag(contentheader, 'section')
   });
 
   afterEach(() => {
@@ -28,7 +30,22 @@ describe('ContentHeader component', () => {
   });
 
 
-  it('should generate a h1 tag', () => {
-    expect(h1.length).to.equal(1);
+  it('should have a section container', () => {
+    expect(section).to.not.equal(null)
+    expect(section.className).to.equal('content-header')
+    expect(section.childNodes.length).to.equal(2)
+    expect(section.childNodes[0].tagName).to.equal('H1')
+    expect(section.childNodes[1].tagName).to.equal('OL')
   });
+
+  it('should generate a small tag with subtitle into h1', () => {
+    expect(small).to.not.equal(null)
+    expect(small.innerHTML).to.equal(data['subtitle'])
+  });
+
+  it('should generate a h1 tag', () => {
+    expect(h1).to.not.equal(null)
+    expect(h1.childNodes[0].innerHTML).to.be.equal(data['title'])
+  });
+
 });
